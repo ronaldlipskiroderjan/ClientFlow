5ba5ee022d8046749966c460210e411fa00ba130
Bernardo <bernardoroche586@gmail.com>
2026-04-01 18:00:50 -0300

    mudança no link do css

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard - ClientFlow</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
