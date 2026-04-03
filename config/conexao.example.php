f7278494a6da68a3d35ffa006d272fc2edfd19bc
Ronald Lipski Roderjan <ronaldlipski1@gmail.com>
2026-04-03 13:04:58 -0300

    feat: adiciona banco de dados e conexão inicial

---
+++ config/conexao.example.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'clientflow';
+$user = '';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Erro de conexão: " . $e->getMessage());
+}
+?>
---
+++ config/conexão.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'clientflow';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+-
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    die("Erro crítico na conexão com o banco de dados: " . $e->getMessage());
+}
+?>
